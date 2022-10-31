abee8afa6cc4e6689848397062367204cebb123c
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-10-31 16:28:36 -0400

    Add GetClosestNodePosition() to CardNodes

--- environment/CardNodes.cs
+++ environment/CardNodes.cs
@@ -1,25 +1,34 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
-public class Card_Nodes : Node2D
+public class CardNodes : Node2D
 {
+	private Dictionary<String, Vector2> nodePositions = new Dictionary<String, Vector2>();
 	private Vector2 bowPosition;
 	private Vector2 portPosition;
 	private Vector2 starboardPosition;
 	private Vector2 sternPosition;
+	private float MAX_DISTANCE = 100.0f;
 
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		bowPosition = GetNode<Node2D>("Bow_Node").Position;
-		portPosition = GetNode<Node2D>("Port_Node").Position;
-		starboardPosition = GetNode<Node2D>("Bow_Node").Position;
-		sternPosition = GetNode<Node2D>("Bow_Node").Position;
+		nodePositions.Add("bow", GetNode<Node2D>("BowNode").Position);
+		nodePositions.Add("port", GetNode<Node2D>("PortNode").Position);
+		nodePositions.Add("starboard", GetNode<Node2D>("StarboardNode").Position);
+		nodePositions.Add("stern", GetNode<Node2D>("SternNode").Position);
 	}
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+	public Vector2 GetClosestNodePosition(Vector2 cardPosition)
+	{
+		Vector2 currentClosestPosition = new Vector2(0, 0);
+		foreach(KeyValuePair<String, Vector2> position in nodePositions)
+		{
+			if (cardPosition.DistanceTo(position.Value) < MAX_DISTANCE)
+			{
+				currentClosestPosition = position.Value;
+			} 
+		}
+		return currentClosestPosition;
+	}
 }
