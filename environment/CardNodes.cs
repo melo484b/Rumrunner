8e64adbe383d53b0eb7a9da18c7fc9378767000d
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-11-02 14:05:58 -0400

    Update CardNodes.cs to check for collision with Cards

--- environment/CardNodes.cs
+++ environment/CardNodes.cs
@@ -5,18 +5,25 @@ using System.Collections.Generic;
 public class CardNodes : Node2D
 {
 	private Dictionary<String, Vector2> nodePositions = new Dictionary<String, Vector2>();
-	private Vector2 bowPosition;
-	private Vector2 portPosition;
-	private Vector2 starboardPosition;
-	private Vector2 sternPosition;
+	private Node2D bow;
+	private Node2D port;
+	private Node2D starboard;
+	private Node2D stern;
 	private float MAX_DISTANCE = 100.0f;
+	
+	[Signal]
+	public delegate void CardAbove(Vector2 closestPosition);
 
 	public override void _Ready()
 	{
-		nodePositions.Add("bow", GetNode<Node2D>("BowNode").Position);
-		nodePositions.Add("port", GetNode<Node2D>("PortNode").Position);
-		nodePositions.Add("starboard", GetNode<Node2D>("StarboardNode").Position);
-		nodePositions.Add("stern", GetNode<Node2D>("SternNode").Position);
+		bow = GetNode<Node2D>("BowNode");
+		port = GetNode<Node2D>("PortNode");
+		starboard = GetNode<Node2D>("StarboardNode");
+		stern = GetNode<Node2D>("SternNode");
+		nodePositions.Add("bow", bow.Position);
+		nodePositions.Add("port", port.Position);
+		nodePositions.Add("starboard", starboard.Position);
+		nodePositions.Add("stern", stern.Position);
 	}
 
 	public Vector2 GetClosestNodePosition(Vector2 cardPosition)
@@ -31,4 +38,55 @@ public class CardNodes : Node2D
 		}
 		return currentClosestPosition;
 	}
+	
+	void OnBowCardDetectorAreaEntered(object area)
+	{
+		EmitSignal("CardAbove", bow.Position);
+		GD.Print(bow.Position);
+	}
+
+	void OnBowCardDetectorAreaExited(object area)
+	{
+		
+	}
+
+
+	void OnPortCardDetectorAreaEntered(object area)
+	{
+		EmitSignal("CardAbove", port.Position);
+		GD.Print(port.Position);
+	}
+
+
+	void OnPortCardDetectorAreaExited(object area)
+	{
+		
+	}
+
+
+	void OnStarboardCardDetectorAreaEntered(object area)
+	{
+		EmitSignal("CardAbove", starboard.Position);
+		GD.Print(starboard.Position);
+	}
+
+
+	void OnStarboardCardDetectorAreaExited(object area)
+	{
+		
+	}
+
+
+	void OnSternCardDetectorAreaEntered(object area)
+	{
+		EmitSignal("CardAbove", stern.Position);
+		GD.Print(stern.Position);
+	}
+
+
+	void OnSternCardDetectorAreaExited(object area)
+	{
+		
+	}
+
 }
