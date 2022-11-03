2867293088d81e7464e40a194102e7a01b7b131e
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-11-03 17:09:29 -0400

    Create CardManager

---
+++ card/CardManager.gd
@@ -0,0 +1,11 @@
+extends Node
+
+
+
+# Card manager talks to DeckManager when drawing cards
+# Card manager reveals and places cards in the scene on start and draws
+# Card manager resets and hides cards after a turn
+
+func _ready():
+	pass
+
