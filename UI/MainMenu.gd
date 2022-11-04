d7aca4dff2fa6fb6e952bc9156a90f204d12244f
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-11-04 18:24:04 -0400

    Update MainMenu.gd
    MainMenu buttons now navigate to the appropriate scenes

--- UI/MainMenu.gd
+++ UI/MainMenu.gd
@@ -6,22 +6,20 @@ onready var options: Button = $ThemedMarginContainer/PanelContainer/MarginContai
 onready var quit: Button = $ThemedMarginContainer/PanelContainer/MarginContainer/VBoxContainer/Quit
 
 
-func _ready():
+func _ready() -> void:
 	new_game.connect("pressed", self, "_on_new_game_button")
 	load_game.connect("pressed", self, "_on_load_game_button")
 	options.connect("pressed", self, "_on_options_button")
 	quit.connect("pressed", self, "_on_quit_button")
 
 func _on_new_game_button() -> void:
-	pass
-	# get_tree().change_scene("new game scene")
+	get_tree().change_scene("res://UI/NewGame.tscn")
 
 func _on_load_game_button() -> void:
-	pass
-	# get_tree().change_scene("load game scene")
+	get_tree().change_scene("res://UI/LoadGame.tscn")
 
 func _on_options_button() -> void:
-	pass
+	get_tree().change_scene("res://UI/OptionsMenu.tscn")
 
 func _on_quit_button() -> void:
 	get_tree().quit()
