636d7579d129c4471c699b5ab22d8c175fdd6b04
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-11-04 18:12:28 -0400

    Create NewGame
    NewGame allows players to enter their name and select a save slot or override an existing slot

---
+++ UI/NewGame.gd
@@ -0,0 +1,36 @@
+extends CanvasLayer
+
+onready var save_1: PanelContainer = $SaveThemedMarginContainer/PanelContainer/MarginContainer/HBoxContainer2/VBoxContainer/SaveFile1
+onready var save_2: PanelContainer = $SaveThemedMarginContainer/PanelContainer/MarginContainer/HBoxContainer2/VBoxContainer2/SaveFile2
+onready var save_3: PanelContainer = $SaveThemedMarginContainer/PanelContainer/MarginContainer/HBoxContainer2/VBoxContainer3/SaveFile3
+onready var name_line: LineEdit = $NameThemedMarginContainer/PanelContainer/MarginContainer/VBoxContainer/LineEdit
+
+var save_files: Array
+var player_data: Array
+
+func _ready():
+	save_files = [save_1, save_2, save_3]
+	update_player_data()
+
+func update_player_data() -> void:
+	player_data = GameDatabase.get_all_data_in_table(GameDatabase.PLAYER_TABLE)
+	for save in player_data:
+		var index: int = player_data.find(save)
+		save_files[index].update_player_data(save["id"], save["name"], save["currency"], save["score"])
+		save_files[index].update_labels()
+		save_files[index].set_active()
+
+func _on_SelectSaveFile1_pressed():
+	save_1.temp_name = name_line.text
+	save_1.write_to_slot(true)
+	save_1.commit_player_data()
+
+func _on_SelectSaveFile2_pressed():
+	save_2.temp_name = name_line.text
+	save_2.write_to_slot(true)
+	save_2.commit_player_data()
+	
+func _on_SelectSaveFile3_pressed():
+	save_3.temp_name = name_line.text
+	save_3.write_to_slot(true)
+	save_3.commit_player_data()
