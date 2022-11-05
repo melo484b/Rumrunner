9bc3f7e920e6cbf7dbde06906b2175c90af6382c
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-11-05 12:55:50 -0400

    Fix bug: ConfirmationDialog was being skipped
    Calling get_tree().change_scene("res://environment/GameEnvironment.tscn") was changing the scene before the player got a chance to confirm changes. Since changes were only being written to the database after receiving the confirmed() signal, no changes were being written to the database.
    
    The current solution is tying the scene change to a separate confirmation button that the player clicks after choosing their save file.

--- UI/NewGame.gd
+++ UI/NewGame.gd
@@ -1,5 +1,7 @@
 extends CanvasLayer
 
+signal write_to_slot
+
 onready var save_1: PanelContainer = $SaveThemedMarginContainer/PanelContainer/MarginContainer/HBoxContainer2/VBoxContainer/SaveFile1
 onready var save_2: PanelContainer = $SaveThemedMarginContainer/PanelContainer/MarginContainer/HBoxContainer2/VBoxContainer2/SaveFile2
 onready var save_3: PanelContainer = $SaveThemedMarginContainer/PanelContainer/MarginContainer/HBoxContainer2/VBoxContainer3/SaveFile3
@@ -7,12 +9,15 @@ onready var name_line: LineEdit = $NameThemedMarginContainer/PanelContainer/Marg
 
 var save_files: Array
 var player_data: Array
+var current_save: int
+
+const NEW_GAME: bool = true
 
 func _ready() -> void:
 	save_files = [save_1, save_2, save_3]
-	update_player_data()
+	display_player_data_from_database()
 
-func update_player_data() -> void:
+func display_player_data_from_database() -> void:
 	player_data = GameDatabase.get_all_data_in_table(GameDatabase.PLAYER_TABLE)
 	for save in player_data:
 		var index: int = player_data.find(save)
@@ -22,18 +27,23 @@ func update_player_data() -> void:
 	
 func _on_SelectSaveFile1_pressed() -> void:
 	save_1.temp_name = name_line.text
-	save_1.write_to_slot(true)
-	save_1.commit_player_data()
-	get_tree().change_scene("res://environment/GameEnvironment.tscn")
+	save_1.check_for_overwrite()
+	save_1.set_player_data_to_new_player_data()
+	current_save = 0
 
 func _on_SelectSaveFile2_pressed() -> void:
-	save_2.temp_name = name_line.text
-	save_2.write_to_slot(true)
-	save_2.commit_player_data()
-	get_tree().change_scene("res://environment/GameEnvironment.tscn")
+	save_2.check_for_overwrite()
+	save_2.set_player_data_to_new_player_data()
+	current_save = 1
 	
 func _on_SelectSaveFile3_pressed() -> void:
-	save_3.temp_name = name_line.text
-	save_3.write_to_slot(true)
-	save_3.commit_player_data()
+	save_3.check_for_overwrite()
+	save_3.set_player_data_to_new_player_data()
+	current_save = 2
+
+func _on_ConfirmationButton_pressed():
+	if save_files[current_save].is_active():
+		save_files[current_save].overwrite_player_data()
+	else:
+		save_files[current_save].write_player_data()
 	get_tree().change_scene("res://environment/GameEnvironment.tscn")
--- UI/SaveFile.gd
+++ UI/SaveFile.gd
@@ -1,5 +1,8 @@
 extends PanelContainer
 
+signal data_written
+signal write_new_file
+
 onready var player_name: Label = $VBoxContainer2/PlayerName
 onready var currency: Label = $VBoxContainer2/Currency
 onready var score: Label = $VBoxContainer2/Score
@@ -10,10 +13,11 @@ export var exported_player_id: int = 1
 const HOVERED_MODULATE = "00ffe6"
 const NORMAL_MODULATE = "ffffff"
 const DEACTIVATED_MODULATE = "111111"
+const LOADED_MODULATE = "1a0095"
 
 var active: bool = false
 var selected: bool = false
-var new_game: bool = false
+var new_game: bool
 var temp_name: String = ""
 
 var save_data: Dictionary = {
@@ -23,7 +27,7 @@ var save_data: Dictionary = {
 	"score": 0
 }
 	
-func _ready():
+func _ready() -> void:
 	set_inactive()
 	initialize_player_id()
 	
@@ -31,7 +35,7 @@ func initialize_player_id() -> void:
 	save_data["player_id"] = exported_player_id
 
 func set_player_data_to_new_player_data() -> void:
-	save_data["player_name"] = temp_name
+	save_data["player_name"] = self.temp_name
 	save_data["currency"] = 0
 	save_data["score"] = 0
 
@@ -54,20 +58,30 @@ func set_active() -> void:
 	active = true
 	modulate = NORMAL_MODULATE
 
+func is_active() -> bool:
+	print("checking for activity: " + str(self.active))
+	return self.active
+
 func commit_player_data() -> void:
 	Player.player_id = save_data["player_id"]
 	Player.player_name = save_data["player_name"]
 	Player.currency = save_data["currency"]
 	Player.score = save_data["score"]
 
-func write_to_slot(is_new_game: bool) -> void:
-	new_game = is_new_game
-	if active:
-		confirmation.show()
-	else:
-		check_new_game()
-		write_player_data()
+func write_to_slot() -> void:
+	commit_player_data()
+	write_player_data()
 
+func overwrite_slot() -> void:
+	commit_player_data()
+	overwrite_player_data()
+
+func check_for_overwrite() -> void:
+	if is_active():
+		confirmation.popup_centered()
+	else:
+		print("not active")
+		
 func overwrite_player_data() -> void:
 	GameDatabase.update_player_data(save_data["player_id"], save_data["player_name"], save_data["currency"], save_data["score"])
 
@@ -75,8 +89,17 @@ func write_player_data() -> void:
 	GameDatabase.add_new_player(save_data["player_name"], save_data["currency"], save_data["score"])
 
 func check_new_game() -> void:
-	if (new_game):
+	if (self.new_game):
 		set_player_data_to_new_player_data()
+
+func set_new_game(is_new_game: bool):
+	self.new_game = is_new_game
+
+func show_file_loaded() -> void:
+	self_modulate = LOADED_MODULATE
+
+func show_file_not_loaded() -> void:
+	self_modulate = NORMAL_MODULATE
 
 func _on_SaveFile_mouse_entered() -> void:
 	if active:
@@ -87,5 +110,9 @@ func _on_SaveFile_mouse_exited() -> void:
 		modulate = NORMAL_MODULATE
 
 func _on_ConfirmationDialog_confirmed() -> void:
-	check_new_game()
-	overwrite_player_data()
+	commit_player_data()
+	update_labels()
+
+
+func _on_ConfirmationDialog_about_to_show():
+	print("about to show")
