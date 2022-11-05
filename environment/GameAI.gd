c49b4585cbc5c4258da60118aabd1854001c5a69
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-11-05 15:30:24 -0400

    Update GameAI.gd

--- environment/GameAI.gd
+++ environment/GameAI.gd
@@ -1,11 +1,10 @@
 extends Node
 
-onready var deck_manager: Node = $"../DeckManager"
 onready var card_manager: Node = $"../CardManager"
 
+enum { PLAYER_TURN, AI_TURN }
+
 func _ready() -> void:
-	
-	if deck_manager == null:
-		push_error("deck_manager not found!")
 	if card_manager == null:
 		push_error("card_manager not found!")
+
