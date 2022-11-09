a3417ab586b38f93223dd4c63aacc92552cf62a2
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-11-09 16:29:48 -0500

    Update CardNode.gd

--- environment/CardNode.gd
+++ environment/CardNode.gd
@@ -3,19 +3,26 @@ extends Position2D
 
 var selected: bool = false
 var filled: bool = false
+export var active: bool = true
 
 func _ready() -> void:
 	deselect()
 
 func select() -> void:
 	modulate = Color.aqua
-	selected = true
-	filled = true
+	self.selected = true
+	self.filled = true
 
 func deselect() -> void:
 	modulate = Color.white
-	selected = false
-	filled = false
+	self.selected = false
+	self.filled = false
+
+func set_inactive() -> void:
+	self.active = false
+
+func set_active() -> void:
+	self.active = true
 
 func _on_Area2D_area_exited(_area) -> void:
 	deselect()
