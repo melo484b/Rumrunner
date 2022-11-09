ba3d1f2cc85b70a63d739327b79f5801ecbebe55
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-11-09 15:43:22 -0500

    Remove comment

--- UI/MainMenu.gd
+++ UI/MainMenu.gd
@@ -5,11 +5,6 @@ onready var load_game: Button = $ThemedMarginContainer/PanelContainer/MarginCont
 onready var options: Button = $ThemedMarginContainer/PanelContainer/MarginContainer/VBoxContainer/Options
 onready var quit: Button = $ThemedMarginContainer/PanelContainer/MarginContainer/VBoxContainer/Quit
 
-# TODO investigate error:
-# E 0:00:01.952   connect: In Object of type 'PanelContainer': Attempt to connect nonexistent signal 'write_new_file' to method 'PanelContainer._on_SaveFile_write_new_file'.
-#  <C++ Error>   Condition "!signal_is_valid" is true. Returned: ERR_INVALID_PARAMETER
-#  <C++ Source>  core/object.cpp:1468 @ connect()
-#  <Stack Trace> MainMenu.gd:20 @ _on_load_game_button()
 func _ready() -> void:
 	new_game.connect("pressed", self, "_on_new_game_button")
 	load_game.connect("pressed", self, "_on_load_game_button")
