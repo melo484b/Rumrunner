0ea4715448fa2ae69fd0bcd12237ec995770960a
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-11-10 14:45:22 -0500

    Create LevelProgress scene

--- UI/LevelProgress.gd
+++ UI/LevelProgress.gd
@@ -1,19 +1,40 @@
 extends MarginContainer
 
 
-const VALUE_INCREMENT: int = 10
+const MAX_VALUE: int = 1200
+const LEVEL_SECONDS: float = 120.0
+
+var time_left: int
 
 onready var progress_bar: TextureProgress = $ProgressTexture
 onready var tween: Tween = $ProgressTween
+onready var timer: Timer = $Timer
+
+
+func _ready():
+	start_level_progress_bar()
 
 
 func add_progress(progress: int) -> void:
-	var current_progress: int = progress_bar.value
-	tween.interpolate_property(progress_bar, "value", current_progress, current_progress + progress, 0.99, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+	var current_time: float = tween.tell()
+	progress_bar.value += progress
+	if tween.is_active():
+		tween.seek(current_time + float(progress/10))
+
+
+func start_level_progress_bar() -> void:
+	tween.interpolate_property(progress_bar, "value", 0, MAX_VALUE, LEVEL_SECONDS, Tween.TRANS_LINEAR, Tween.EASE_OUT)
 	if not tween.is_active():
 		tween.start()
+	timer.start()
 
 
-func _on_ProgressTimer_timeout() -> void:
-	if progress_bar.value < progress_bar.max_value:
-		add_progress(self.VALUE_INCREMENT)
+func stop_level_progress_bar() -> int:
+	var time_left: int = int(timer.time_left)
+	timer.stop()
+	return time_left
+
+
+func _on_ProgressTexture_value_changed(value):
+	if value >= MAX_VALUE:
+		time_left = stop_level_progress_bar()
