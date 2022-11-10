79963349c9bafba4601c90417d13a0710746c266
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-11-10 10:35:54 -0500

    Implement level progress bar

--- UI/GameOverlay.gd
+++ UI/GameOverlay.gd
@@ -4,6 +4,8 @@ extends CanvasLayer
 signal level_complete
 
 const MAX_LEVEL_TIME: int = 120
+const TIME_DECREMENT: int = 1
+const TIME_MINIMUM: int = 0
 
 var level_time: int = MAX_LEVEL_TIME
 var time_left: int
@@ -25,11 +27,16 @@ func remove_time(time_to_remove: int) -> void:
 
 
 func _on_ProgressTimer_timeout() -> void:
-	if self.level_time > 0:
-		self.level_time -= 1
-	else:
-		progress_timer.stop()
-		time_left = MAX_LEVEL_TIME - level_time
-		emit_signal("level_complete", time_left)
+	if self.level_time > TIME_MINIMUM:
+		self.level_time -= TIME_DECREMENT
+	elif self.level_time <= TIME_MINIMUM:
+		self.level_time = TIME_MINIMUM
 	$DeleteMeAfterTesting.text = str(self.level_time)
 	
+
+
+func _on_ProgressTexture_value_changed(value):
+	if value >= self.MAX_LEVEL_TIME:
+		progress_timer.stop()
+		emit_signal("level_complete", self.level_time)
+		print("level complete" + str(self.level_time))
--- UI/LevelProgress.gd
+++ UI/LevelProgress.gd
@@ -1,9 +1,20 @@
 extends MarginContainer
 
 
+const VALUE_MAX: int = 120
+const VALUE_INCREMENT: int = 10
+
 onready var progress_bar: TextureProgress = $ProgressTexture
+onready var tween: Tween = $ProgressTween
 
 
-func _on_ProgressTimer_timeout():
-	if progress_bar.value < 120:
-		progress_bar.value += 1
+func add_progress(progress: int) -> void:
+	var current_progress: int = progress_bar.value
+	tween.interpolate_property(progress_bar, "value", current_progress, current_progress + progress, 0.6, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	if not tween.is_active():
+		tween.start()
+
+
+func _on_ProgressTimer_timeout() -> void:
+	if progress_bar.value < self.VALUE_MAX:
+		add_progress(self.VALUE_INCREMENT)
