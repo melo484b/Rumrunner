68245bbad2582caa7303919d5390bfc6d040697f
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-11-11 14:37:33 -0500

    Remove turn logic from IncomingAnnouncement

--- UI/IncomingAnnouncement.gd
+++ UI/IncomingAnnouncement.gd
@@ -16,8 +16,3 @@ func display_incoming() -> void:
 func _on_Timer_timeout() -> void:
 	self.visible = false
 	emit_signal("player_turn")
-
-
-func _on_TurnTimer_player_time_out() -> void:
-	emit_signal("incoming")
-	display_incoming()
