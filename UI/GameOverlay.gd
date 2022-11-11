5a5a3cba21506d461ae021b4c8cef5ce43b502fe
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-11-11 14:37:07 -0500

    Remove turn logic from GameOverlay

--- UI/GameOverlay.gd
+++ UI/GameOverlay.gd
@@ -2,32 +2,25 @@ extends CanvasLayer
 
 
 signal level_complete
-signal player_turn
-signal ai_turn
 
 onready var deck: TextureRect = $CardBackTexture
 onready var level_progress_bar: MarginContainer = $LevelProgress
-onready var incoming_announcement: MarginContainer = $IncomingAnnouncement
-onready var turn_timer: MarginContainer = $TurnTimer
 onready var input_blocker: Panel = $InputBlocker
 
 
 func _ready() -> void:
 	level_progress_bar.start_level_progress_bar()
-	display_incoming_announcement()
 
 
 func draw_card_animation() -> void:
 	deck.draw_animation()
 
 
-func display_incoming_announcement() -> void:
-	input_blocker.visible = true
-	incoming_announcement.display_incoming()
-
-
-func start_player_timer() -> void:
-	turn_timer.start_timer()
+func toggle_player_input() -> void:
+	if input_blocker.visible == true:
+		input_blocker.visible = false
+	else:
+		input_blocker.visible = true
 
 
 func _on_ProgressTexture_value_changed(value) -> void:
@@ -36,13 +29,3 @@ func _on_ProgressTexture_value_changed(value) -> void:
 		emit_signal("level_complete")
 
 
-func _on_IncomingAnnouncement_incoming() -> void:
-	input_blocker.visible = true
-	emit_signal("ai_turn")
-	
-
-
-func _on_IncomingAnnouncement_player_turn() -> void:
-	input_blocker.visible = false
-	start_player_timer()
-	emit_signal("player_turn")
