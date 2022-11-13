f705e67ea67474b7fb4ff85b93f10926260297ff
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-11-12 19:20:05 -0500

    Update GameAI.gd

--- environment/GameAI.gd
+++ environment/GameAI.gd
@@ -3,7 +3,8 @@ extends Node
 
 enum Turn { PLAYER, AI }
 
-var current_turn: int = Turn.AI
+
+var current_rum: int = 20 setget set_current_rum
 var player_card_positions: Dictionary = {
 	"bow": null,
 	"port": null,
@@ -24,6 +25,7 @@ onready var ai_card_manager: Node = $"../AiCardManager"
 onready var card_manager: Node = $"../CardManager"
 onready var incoming: MarginContainer = $"../IncomingAnnouncement"
 onready var turn_timer: MarginContainer = $"../TurnTimer"
+onready var score_counter: MarginContainer = $"../ScoreCounter"
 
 
 func _ready() -> void:
@@ -32,9 +34,8 @@ func _ready() -> void:
 
 
 func start_ai_turn() -> void:
-	current_turn = Turn.AI
 	ai_card_manager.draw_hand()
-	# AI places crd/cards
+	ai_card_manager.place_card()
 	show_incoming_banner()
 	
 
@@ -43,67 +44,62 @@ func show_incoming_banner():
 
 
 func start_player_turn() -> void:
-	current_turn = Turn.PLAYER
-	overlay.toggle_player_input()
+	overlay.unblock_player_input()
 	turn_timer.start_timer()
 
 
 func resolve_cards() -> void:
-	overlay.toggle_player_input()
+	overlay.block_player_input()
 	# Check for cards placed by AI
 	# Check for cards placed by Player
 	# Activate/resolve card effects
-	# start_ai_turn()
+	ai_card_manager.discard_hand()
+	start_ai_turn()
+
+
+func set_current_rum(new_rum: int) -> void:
+	current_rum = new_rum
+	score_counter.set_score_counter_label(current_rum)
 
 
 func _on_Card_placed(card_data, node) -> void:
 	player_card_positions[node] = card_data
-	print(player_card_positions)
 
 
 func _on_Card2_placed(card_data, node) -> void:
 	player_card_positions[node] = card_data
-	print(player_card_positions)
 
 
 func _on_Card3_placed(card_data, node) -> void:
 	player_card_positions[node] = card_data
-	print(player_card_positions)
 
 
 func _on_Card4_placed(card_data, node) -> void:
 	player_card_positions[node] = card_data
-	print(player_card_positions)
 
 
 func _on_Card5_placed(card_data, node) -> void:
 	player_card_positions[node] = card_data
-	print(player_card_positions)
 
 
 func _on_AiCard_placed(card_data, node) -> void:
 	ai_card_positions[node] = card_data
-	print(ai_card_positions)
 
 
 func _on_AiCard2_placed(card_data, node) -> void:
 	ai_card_positions[node] = card_data
-	print(ai_card_positions)
 
 
 func _on_AiCard3_placed(card_data, node) -> void:
 	ai_card_positions[node] = card_data
-	print(ai_card_positions)
 
 
 func _on_AiCard4_placed(card_data, node) -> void:
 	ai_card_positions[node] = card_data
-	print(ai_card_positions)
 
 
 func _on_AiCard5_placed(card_data, node) -> void:
 	ai_card_positions[node] = card_data
-	print(ai_card_positions)
 
 
 func _on_TurnTimer_player_time_out():
@@ -118,3 +114,8 @@ func _on_GameOverlay_end_player_turn():
 	var extra_time: int = turn_timer.stop_timer()
 	overlay.add_progress(extra_time)
 	resolve_cards()
+	card_manager.discard_hand()
+
+
+func _on_AiCardManager_ai_ready():
+	start_ai_turn()
