eac90fc632ebe863b2dce1eff3d67a3be8edd596
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-11-13 17:48:35 -0500

    Update GameOverlay to correctly modulate EndTurnButton

--- UI/GameOverlay.gd
+++ UI/GameOverlay.gd
@@ -5,10 +5,13 @@ signal start_level
 signal level_complete
 signal end_player_turn
 
+const INACTIVE_BUTTON: String = "4d5482"
+const ACTIVE_BUTTON: String = "ffffff"
 
 onready var deck: TextureRect = $CardBackTexture
 onready var level_progress_bar: MarginContainer = $LevelProgress
 onready var input_blocker: Panel = $InputBlocker
+onready var end_turn_button: Button = $EndTurnButton
 
 
 func _ready() -> void:
@@ -23,10 +26,12 @@ func draw_card_animation() -> void:
 
 func block_player_input():
 	input_blocker.visible = true
+	end_turn_button.modulate = INACTIVE_BUTTON
 
 
 func unblock_player_input() -> void:
 	input_blocker.visible = false
+	end_turn_button.modulate = ACTIVE_BUTTON
 
 
 func add_progress(progress: int) -> void:
@@ -39,5 +44,5 @@ func _on_ProgressTexture_value_changed(value) -> void:
 		emit_signal("level_complete")
 
 
-func _on_GameButton_pressed():
+func _on_EndTurnButton_pressed():
 	emit_signal("end_player_turn")
