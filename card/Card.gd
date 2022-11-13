b03f4d0e7f0de7ac061c229da7a7263d6e8a3231
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-11-13 17:08:40 -0500

    Refactor card movement

--- card/Card.gd
+++ card/Card.gd
@@ -12,18 +12,16 @@ const CARD_CLASS = {
 }
 const CARD_ART_PATH = "res://card_art/"
 
-export var selectable: bool = true
-
+var selectable: bool = true
 var card_data: Dictionary
 var selected: bool = false
 var placed: bool = false
 var underway: bool = false
 var target_position: Vector2
 var original_position: Vector2
-var assigned_position: Vector2
 var target_nodes: Array = []
 
-onready var collider: CollisionShape2D = $CardCollider
+onready var card_collider: CollisionShape2D = $CardCollider
 onready var animator: AnimationPlayer = $AnimationPlayer
 onready var area: Area2D = $Sprite/Area2D
 onready var card_name: Label = $ThemedMarginContainer/ClassPanelContainer/MarginContainer/VBoxContainer/Name
@@ -35,10 +33,13 @@ onready var defense: Label = $ThemedMarginContainer/ClassPanelContainer/MarginCo
 
 
 func _ready() -> void:
+	_on_ready()
+
+
+func _on_ready() -> void:
 	target_nodes = get_tree().get_nodes_in_group("CARD_NODE")
 	target_position = global_position
 	original_position = global_position
-	assigned_position = original_position
 
 
 func _input(event) -> void:
@@ -64,8 +65,6 @@ func _input(event) -> void:
 
 func _integrate_forces(state):
 	drag(state)
-	if underway:
-		state.set_transform(assign_position())
 
 
 func drag(state: Physics2DDirectBodyState) -> void:
@@ -83,43 +82,33 @@ func drag(state: Physics2DDirectBodyState) -> void:
 		state.set_transform(card_transform)
 
 
-func assign_position() -> Transform2D:
-	var card_transform: Transform2D = get_transform()
-	card_transform.origin.x = assigned_position.x
-	card_transform.origin.y = assigned_position.y
-	return card_transform
-
-
-func set_assigned_position(new_assigned_position: Vector2) -> void:
-	assigned_position = new_assigned_position
-	
-
-func set_underway(underway_status: bool):
-	underway = underway_status
+func set_target_position(new_target_position: Vector2) -> void:
+	print("before setting position: " + str(target_position))
+	target_position = new_target_position
+	print("new position: " + str(target_position))
 
 
 func set_placed(node_name: String) -> void:
 	placed = true
-	collider.disabled = true
+	card_collider.disabled = true
 	animator.stop()
 	emit_signal("placed", card_data, node_name)
 	print(self.name + " placed")
 
 
 func reset_card() -> void:
-	set_underway(true)
-	assigned_position = original_position
+	target_position = original_position
 	placed = false
-	collider.disabled = false
 	if visible == false:
 		visible = true
+	yield(get_tree().create_timer(0.2), "timeout")
+	card_collider.disabled = false
 
 
 func set_card_data(new_data: Dictionary) -> void:
 	if !new_data.empty():
 		card_data.merge(new_data, true)
 	else:
-		set_placed("none")
 		visible = false
 
 
@@ -131,8 +120,8 @@ func populate_card_data() -> void:
 	set_card_stats(card_data.get("offensive_stat", -1), card_data.get("defensive_stat", -1))
 
 
-func set_card_name(card_name: String) -> void:
-	self.card_name.text = card_name
+func set_card_name(new_card_name: String) -> void:
+	card_name.text = new_card_name
 
 
 func set_card_class(class_modulation: String) -> void:
@@ -144,13 +133,13 @@ func set_card_art(art_id: int) -> void:
 	art.texture = load(art_path)
 
 
-func set_card_description(description: String) -> void:
-	self.description.text = description
+func set_card_description(new_description: String) -> void:
+	description.text = new_description
 
 
-func set_card_stats(offense: int, defense: int) -> void:
-	self.offense.text = str(offense)
-	self.defense.text = str(defense)
+func set_card_stats(new_offense: int, new_defense: int) -> void:
+	offense.text = str(new_offense)
+	defense.text = str(new_defense)
 
 
 func get_id() -> int:
@@ -160,5 +149,3 @@ func get_id() -> int:
 func _on_Area2D_input_event(_viewport, _event, _shape_idx) -> void:
 	if Input.is_action_just_pressed("left_click") and not placed:
 		selected = true
-	if selectable:
-		underway = false
