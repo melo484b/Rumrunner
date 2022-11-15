d2031a1ad60a4f7e585d40e253eb5cb81345d5fb
Dylan <95886475+MeloDev484b@users.noreply.github.com>
2022-11-14 19:27:18 -0500

    Add particle effects and animations to ScoreCounter

--- UI/ScoreCounter.gd
+++ UI/ScoreCounter.gd
@@ -2,7 +2,16 @@ extends MarginContainer
 
 
 onready var counter: Label = $PanelContainer/MarginContainer/CenterContainer/ScoreLabel
+onready var rum_splash: Particles2D = $RumSplash
+onready var animator: AnimationPlayer = $AnimationPlayer
 
 
 func update_score(new_score: int) -> void:
+	if int(counter.text) > new_score:
+		animator.play("lose_rum")
+	elif int(counter.text) < new_score:
+		animator.play("gain_rum")
+	else:
+		animator.play("no_change")
 	counter.text = str(new_score)
+	rum_splash.restart()
