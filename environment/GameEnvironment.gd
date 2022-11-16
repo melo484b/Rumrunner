023b392c5b86083cda65d8325f25f8abcef78493
Dylan <95886475+melo484b@users.noreply.github.com>
2022-11-15 20:11:11 -0500

    Connect level complete signal

--- environment/GameEnvironment.gd
+++ environment/GameEnvironment.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
 
-func _ready() -> void:
-	pass
+onready var overlay: CanvasLayer = $GameOverlay
+onready var ai: Node = $GameAI
 
 
-func _on_GameOverlay_level_complete():
-	pass
+func _on_GameOverlay_level_complete() -> void:
+	overlay.set_score_tally_rum(ai.current_rum)
+	overlay.tally_score()
--- environment/ShipNodeManager.gd
+++ environment/ShipNodeManager.gd
@@ -11,3 +11,10 @@ func reset_ship_nodes():
 
 func _on_CardManager_player_hand_discarded():
 	reset_ship_nodes()
+
+
+func _on_GameOverlay_level_complete():
+	var children: Array = get_children()
+	for child in children:
+		child.visible = false
+		child.set_inactive()
