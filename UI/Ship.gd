32437ed0e6956569d3d610488c3ab8271bed9e23
Dylan <95886475+melo484b@users.noreply.github.com>
2022-11-17 11:32:50 -0500

    Connect level_complete signal to Ship.gd

--- UI/Ship.gd
+++ UI/Ship.gd
@@ -4,3 +4,7 @@ onready var animator: AnimationPlayer = $AnimationPlayer
 
 func _ready():
 	animator.play("sway_port_starboard")
+
+
+func _on_GameOverlay_level_complete():
+	animator.play("level_success")
