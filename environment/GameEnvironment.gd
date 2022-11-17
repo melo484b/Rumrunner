2b69e634ef417d769051b6abf793ae59fb6bafda
Dylan <95886475+melo484b@users.noreply.github.com>
2022-11-17 16:50:07 -0500

    Update GameEnvironment.gd

--- environment/GameEnvironment.gd
+++ environment/GameEnvironment.gd
@@ -8,3 +8,7 @@ onready var ai: Node = $GameAI
 func _on_GameOverlay_level_complete() -> void:
 	overlay.set_score_tally_rum(ai.current_rum)
 	overlay.tally_score()
+
+
+func _on_GameOverlay_change_scene_to_shop():
+	get_tree().change_scene("res://UI/CardShop.tscn")
