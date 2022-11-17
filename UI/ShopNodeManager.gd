4c7c05367f03611f6db5319e57c468f6968da672
Dylan <95886475+melo484b@users.noreply.github.com>
2022-11-17 16:49:24 -0500

    Update ShopNodeManager.gd

--- UI/ShopNodeManager.gd
+++ UI/ShopNodeManager.gd
@@ -6,6 +6,11 @@ onready var cost_display_2: MarginContainer = $CardCostDisplay2
 onready var cost_display_3: MarginContainer = $CardCostDisplay3
 onready var cost_display_4: MarginContainer = $CardCostDisplay4
 onready var cost_display_5: MarginContainer = $CardCostDisplay5
+onready var player_currency: Label = $PlayerCurrencyContainer/PlayerCurrencyDisplay/PanelContainer/MarginContainer/CenterContainer/Label
+
+
+func _ready():
+	player_currency.text = "Currency: " + str(Player.currency)
 
 
 func process_transaction() -> void:
@@ -13,24 +18,29 @@ func process_transaction() -> void:
 
 
 func _on_ShopNode_occupied_by_card(card_data):
-	cost_display_1.set_price(card_data["cost"])
+	if !card_data.empty():
+		cost_display_1.set_price(card_data["cost"])
 
 
 func _on_ShopNode2_occupied_by_card(card_data):
-	cost_display_2.set_price(card_data["cost"])
+	if !card_data.empty():
+		cost_display_2.set_price(card_data["cost"])
 
 
 func _on_ShopNode3_occupied_by_card(card_data):
-	cost_display_3.set_price(card_data["cost"])
+	if !card_data.empty():
+		cost_display_3.set_price(card_data["cost"])
 
 
 func _on_ShopNode4_occupied_by_card(card_data):
-	cost_display_4.set_price(card_data["cost"])
+	if !card_data.empty():
+		cost_display_4.set_price(card_data["cost"])
 
 
 func _on_ShopNode5_occupied_by_card(card_data):
-	cost_display_5.set_cost(card_data["cost"])
+	if !card_data.empty():
+		cost_display_5.set_cost(card_data["cost"])
 
 
 func _on_PurchaseButton_pressed():
-	pass # Replace with function body.
+	process_transaction()
