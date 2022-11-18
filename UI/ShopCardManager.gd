0406edbaf837b68683bc01b9cdf6382bd74b3b28
Dylan <95886475+melo484b@users.noreply.github.com>
2022-11-18 16:26:46 -0500

    Extend CardManager

--- UI/ShopCardManager.gd
+++ UI/ShopCardManager.gd
@@ -1,5 +1,38 @@
-extends Node
+extends CardManager
+
+
+const SHOP_ID: int = 99
+
+func _init() -> void:
+	owner_id = SHOP_ID
+
+
+func _on_ready() -> void:
+	rng.randomize()
+	card_1 = $Card
+	card_2 = $Card2
+	card_3 = $Card3
+	card_4 = $Card4
+	card_5 = null
+	cards = [ card_1, card_2, card_3, card_4 ]
+	build_deck(owner_id)
+	draw_hand()
+
+
+func draw_hand() -> void:
+	for card in cards:
+		card.animator.play("shop_shuffle")
+		card.call("set_card_data", draw_card())
+		card.call("populate_card_data")
+
+
+func discard_hand() -> void:
+	for card in cards:
+		discard(card.get_id())
+		card.reset_card()
+	shuffle_cards()
 
 
 func _on_RerollButton_pressed():
-	pass # Pick new shop cards
+	discard_hand()
+	draw_hand()
