165fdbf747b63ae855d574ee10db0b7d985a8063
Dylan <95886475+melo484b@users.noreply.github.com>
2022-11-21 16:27:50 -0500

    Update ShopNode.gd card selection method

--- environment/ShopNode.gd
+++ environment/ShopNode.gd
@@ -2,6 +2,7 @@ extends CardNode
 
 
 signal occupied_by_card(card_data)
+signal unoccupied
 
 var card_held: Card
 
@@ -9,17 +10,37 @@ onready var cards: Array = get_tree().get_nodes_in_group("CARD")
 
 
 func _ready() -> void:
-	select()
+	_on_ready()
+
+
+func _on_ready():
+	yield(get_tree().root, "ready")
+	get_nearby_card()
 
 
 func select() -> void:
 	modulate = Color.aqua
-	self.selected = true
-	self.filled = true
-	for card in cards:
-		var distance = global_position.distance_to(card.global_position)
-		if distance < 100 and selected:
-			card_held = card
+	selected = true
+	filled = true
 	if card_held != null:
 		emit_signal("occupied_by_card", card_held.card_data)
 		print(name + " occupied by " + str(card_held.name))
+
+
+func deselect() -> void:
+	modulate = Color.white
+	selected = false
+	filled = false
+	emit_signal("unoccupied")
+	
+
+func get_nearby_card() -> void:
+	if card_held == null:
+		card_held = cards[0]
+	for card in cards:
+		if card.global_position.distance_to(self.global_position) < card_held.global_position.distance_to(self.global_position):
+			if card.global_position.distance_to(self.global_position) <= 50:
+				card_held = card
+				select()
+			else:
+				deselect()
