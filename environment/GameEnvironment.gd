56350d4b27f9df0c7f5f62cb25f8099d523c8002
Dylan <95886475+melo484b@users.noreply.github.com>
2022-11-23 12:31:06 -0500

    Update GameEnvironment.gd
    - Change scene address to reflect CardShop move from UI to shop directory

--- environment/GameEnvironment.gd
+++ environment/GameEnvironment.gd
@@ -12,4 +12,4 @@ func _on_GameOverlay_level_complete() -> void:
 
 func _on_GameOverlay_change_scene_to_shop() -> void:
 # warning-ignore:return_value_discarded
-	get_tree().change_scene("res://UI/CardShop.tscn")
+	get_tree().change_scene("res://shop/CardShop.tscn")
