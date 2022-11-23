edf658fca24550f02a0c1fbc910e9cab4cf7eb49
Dylan <95886475+melo484b@users.noreply.github.com>
2022-11-23 09:31:32 -0500

    Move CardShop from UI to shop directory

--- shop/CardShop.gd
+++ shop/CardShop.gd
@@ -8,20 +8,35 @@ onready var reroll_cost_display: MarginContainer = $ShopNodeManager/RerollCostDi
 onready var shop_node_manager: Node = $ShopNodeManager
 onready var dice: Sprite = $ShopNodeManager/Dice
 onready var shop_card_manager: Node = $ShopCardManager
+onready var currency_display: CenterContainer = $ShopNodeManager/CurrencyDisplay
+
+
+func _ready() -> void:
+	Player.currency = 100
 
 
 func increase_cost() -> void:
 	reroll_cost = int(reroll_cost * 1.6175)
 
 
-func _on_RerollButton_pressed():
+func process_transaction(transaction_amount):
+	Player.currency -= transaction_amount
+	currency_display.update_label(Player.currency)
+
+
+func _on_RerollButton_pressed() -> void:
 	if Player.currency > reroll_cost:
 		increase_cost()
 		reroll_cost_display.set_cost(reroll_cost)
-		shop_node_manager.refresh_shop_nodes()
 		dice.roll_the_die()
 		shop_card_manager.discard_hand()
 		shop_card_manager.draw_hand()
+		shop_node_manager.refresh_shop_nodes()
+		process_transaction(reroll_cost)
 	else:
 		pass
 		# TODO: Warn player they do not have enough money
+
+
+func _on_ShopNodeManager_transaction_made(amount):
+	process_transaction(amount)
