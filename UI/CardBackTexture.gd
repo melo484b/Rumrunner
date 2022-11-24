e159b6c0dc718c3ec15a7f78219d23d83d154d6c
Dylan <95886475+melo484b@users.noreply.github.com>
2022-11-23 21:14:15 -0500

    Add SFX to CardBackTexture

--- UI/CardBackTexture.gd
+++ UI/CardBackTexture.gd
@@ -3,9 +3,11 @@ extends TextureRect
 
 onready var animator: AnimationPlayer = $AnimationPlayer
 onready var timer: Timer = $Timer
+onready var shuffle_sfx: Node = $CardSFXplayer
 
 
 func draw_animation() -> void:
+	shuffle_sfx.play()
 	animator.play("draw_card")
 	timer.start()
 
