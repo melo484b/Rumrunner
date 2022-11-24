099c96b7f3d4fc41eb567252b5f2d96ee7097816
Dylan <95886475+melo484b@users.noreply.github.com>
2022-11-24 16:26:12 -0500

    Add insufficient_funds signal

--- shop/ShopNodeManager.gd
+++ shop/ShopNodeManager.gd
@@ -2,6 +2,7 @@ extends Node
 
 
 signal transaction_made(amount)
+signal insufficient_funds
 
 onready var shop_node_1: Position2D = $ShopNode
 onready var shop_node_2: Position2D = $ShopNode2
@@ -19,12 +20,13 @@ func _ready() -> void:
 # TODO notify player of transaction then change scene
 func purchase_card() -> void:
 	if shop_node_5.card_held != null:
-		print("purchasing!" + str(shop_node_5.card_held.card_data["name"]))
+		print("attempting to purchase: " + str(shop_node_5.card_held.card_data["name"]))
 		if shop_node_5.card_held.card_data["cost"] <= Player.currency:
 			emit_signal("transaction_made", shop_node_5.card_held.card_data["cost"])
 			var current_copies: int = GameDatabase.get_current_copies(shop_node_5.card_held.card_data["id"], Player.player_id)
 			GameDatabase.associate_new_card_with_player(Player.player_id, shop_node_5.card_held.card_data["id"], current_copies + 1)
-
+		else:
+			emit_signal("insufficient_funds")
 
 func refresh_shop_nodes() -> void:
 	var shop_nodes: Array = get_tree().get_nodes_in_group("CARD_NODE")
