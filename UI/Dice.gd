e56815480565dd90d6b177676e7b696cbf5cc37e
Dylan <95886475+melo484b@users.noreply.github.com>
2022-11-23 21:14:34 -0500

    Update Dice.gd

--- UI/Dice.gd
+++ UI/Dice.gd
@@ -11,10 +11,13 @@ onready var tween: Tween = $Tween
 
 func roll_the_die() -> void:
 	rng.randomize()
+# warning-ignore:return_value_discarded
 	tween.interpolate_property(self, "frame", 0, MAX_FRAME, ROLL_DURATION, Tween.TRANS_LINEAR, Tween.EASE_OUT)
 	if not tween.is_active():
+# warning-ignore:return_value_discarded
 		tween.start()
 	else:
+# warning-ignore:return_value_discarded
 		tween.reset(self, "frame")
 	yield(tween, "tween_completed")
 	self.set_frame(rng.randi_range(0, 5))
