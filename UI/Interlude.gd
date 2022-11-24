a5eb45b7b957e3f4fdab8ddb4076dab0ceee34ee
Dylan <95886475+melo484b@users.noreply.github.com>
2022-11-24 17:07:57 -0500

    Update Interlude.gd

--- UI/Interlude.gd
+++ UI/Interlude.gd
@@ -3,9 +3,11 @@ extends CanvasLayer
 
 func _on_ReturnToMainMenu_pressed() -> void:
 	yield(get_tree().create_timer(0.4), "timeout")
+# warning-ignore:return_value_discarded
 	get_tree().change_scene("res://UI/MainMenu.tscn")
 
 
 func _on_NextRun_pressed() -> void:
 	yield(get_tree().create_timer(0.4), "timeout")
+# warning-ignore:return_value_discarded
 	get_tree().change_scene("res://environment/GameEnvironment.tscn")
