9032067bdb0e49a176613140b08b76c198088582
Dylan <95886475+melo484b@users.noreply.github.com>
2022-11-25 15:19:55 -0500

    Implement ambient waves sound

--- environment/GameEnvironment.gd
+++ environment/GameEnvironment.gd
@@ -5,6 +5,10 @@ onready var overlay: CanvasLayer = $GameOverlay
 onready var ai: Node = $GameAI
 
 
+func _ready() -> void:
+	AmbientWaves.stream_paused = true
+
+
 func _on_GameOverlay_level_complete() -> void:
 	overlay.set_score_tally_rum(ai.current_rum)
 	overlay.tally_score()
@@ -13,4 +17,5 @@ func _on_GameOverlay_level_complete() -> void:
 func _on_GameOverlay_change_scene_to_shop() -> void:
 	yield(get_tree().create_timer(0.4), "timeout")
 # warning-ignore:return_value_discarded
+	AmbientWaves.stream_paused = false
 	get_tree().change_scene("res://shop/CardShop.tscn")
