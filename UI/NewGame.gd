aae827309d98ae992d94dac287f35db37e1f5c01
Dylan <95886475+melo484b@users.noreply.github.com>
2022-12-24 14:57:53 -0500

    Implement validation for player names

--- UI/NewGame.gd
+++ UI/NewGame.gd
@@ -6,6 +6,8 @@ const NEW_GAME: bool = true
 var save_files: Array
 var player_data: Array
 var current_save: int
+var name_validation = RegEx.new()
+var name_is_valid: bool = false
 
 onready var save_1: PanelContainer = $SaveThemedMarginContainer/PanelContainer/MarginContainer/HBoxContainer2/VBoxContainer/SaveFile1
 onready var save_2: PanelContainer = $SaveThemedMarginContainer/PanelContainer/MarginContainer/HBoxContainer2/VBoxContainer2/SaveFile2
@@ -16,6 +18,7 @@ onready var name_line: LineEdit = $NameThemedMarginContainer/PanelContainer/Marg
 func _ready() -> void:
 	save_files = [save_1, save_2, save_3]
 	display_player_data_from_database()
+	name_validation.compile("^[A-Za-z0-9]+$")
 
 
 func display_player_data_from_database() -> void:
@@ -25,31 +28,57 @@ func display_player_data_from_database() -> void:
 		save_files[index].update_player_data(save["id"], save["name"], save["currency"], save["score"])
 		save_files[index].update_labels()
 		save_files[index].set_active()
+
+
+func validate_player_name(name_to_validate: String) -> bool:
+	var is_name_valid: RegExMatch = name_validation.search(name_to_validate)
+	if is_name_valid:
+		return true
+	return false
 	
-	
+
 func _on_SelectSaveFile1_pressed() -> void:
-	save_1.temp_name = name_line.text
-	save_1.check_for_overwrite()
-	save_1.set_player_data_to_new_player_data()
-	current_save = 0
+	if name_is_valid:
+		save_1.temp_name = name_line.text
+		save_1.check_for_overwrite()
+		save_1.set_player_data_to_new_player_data()
+		current_save = 0
+	else:
+		# TODO: Play error sound if the name is not valid
+		print("problem")
 
 
 func _on_SelectSaveFile2_pressed() -> void:
-	save_2.check_for_overwrite()
-	save_2.set_player_data_to_new_player_data()
-	current_save = 1
+	if name_is_valid:
+		save_2.check_for_overwrite()
+		save_2.set_player_data_to_new_player_data()
+		current_save = 1
+	else:
+		# TODO: Play error sound if the name is not valid
+		print("problem")
 	
 	
 func _on_SelectSaveFile3_pressed() -> void:
-	save_3.check_for_overwrite()
-	save_3.set_player_data_to_new_player_data()
-	current_save = 2
+	if name_is_valid:
+		save_3.check_for_overwrite()
+		save_3.set_player_data_to_new_player_data()
+		current_save = 2
+	else:
+		# TODO: Play error sound if the name is not valid
+		print("problem")
 
 
 func _on_ConfirmationButton_pressed() -> void:
-	if save_files[current_save].is_active():
-		save_files[current_save].overwrite_player_data()
-	else:
-		save_files[current_save].write_player_data()
-	yield(get_tree().create_timer(0.4), "timeout")
-	get_tree().change_scene("res://environment/GameEnvironment.tscn")
+	if name_is_valid:
+		if save_files[current_save].is_active():
+			save_files[current_save].overwrite_player_data()
+		else:
+			save_files[current_save].write_player_data()
+		yield(get_tree().create_timer(0.4), "timeout")
+		get_tree().change_scene("res://environment/GameEnvironment.tscn")
+
+
+func _on_LineEdit_text_changed(new_text):
+	name_is_valid = false
+	if validate_player_name(new_text):
+		name_is_valid = true
