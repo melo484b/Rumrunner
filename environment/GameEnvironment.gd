9123bbe8cf48e78fea6e186f3fbcbeec4b4fae36
Dylan <95886475+melo484b@users.noreply.github.com>
2023-01-08 16:33:22 -0500

    Disable Shop scene in game loop
    - Temporary change

--- environment/GameEnvironment.gd
+++ environment/GameEnvironment.gd
@@ -14,8 +14,11 @@ func _on_GameOverlay_level_complete() -> void:
 	overlay.tally_score()
 
 
+# TODO: Change scene to shop - currently changes to interlude
 func _on_GameOverlay_change_scene_to_shop() -> void:
+	GameDatabase.update_player_data(Player.player_id, Player.name, Player.currency, Player.score)
 	yield(get_tree().create_timer(0.4), "timeout")
 # warning-ignore:return_value_discarded
 	AmbientWaves.stream_paused = false
-	get_tree().change_scene("res://shop/CardShop.tscn")
+	#get_tree().change_scene("res://shop/CardShop.tscn")
+	get_tree().change_scene("res://UI/Interlude.tscn")
